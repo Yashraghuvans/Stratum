012bca2aa1bb9220bfcb1d51c1a90af005f6e4fd
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-06-23 18:01:57 +0530

    Initial Commit

---
+++ Stratum/StratumApp.swift
@@ -0,0 +1,17 @@
+//
+//  StratumApp.swift
+//  Stratum
+//
+//  Created by Yash Raghuvanshi on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StratumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
