44dd32b322f69378072510e0c9610348968cb71a
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-06-26 22:12:09 +0530

    Update ContentView.swift

--- Stratum/ContentView.swift
+++ Stratum/ContentView.swift
@@ -1,21 +1,12 @@
-//
-//  ContentView.swift
-//  Stratum
-//
-//  Created by Yash Raghuvanshi on 23/06/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
         }
-        .padding()
+        
     }
 }
 
